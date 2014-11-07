6b331439daafc5ee4b3678668f2594691f1dd30d
Jeremy LEFEBVRE--AGNES <jeremy.la@me.com>
2014-11-07 18:53:41 +0100

    ft_strsub.c

---
+++ new/ft_strsub.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char * ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	int	index;
+	char	*dest;
+
+	index = 0;
+	dest = malloc(sizeof(char) * len + 1);
+	if (len && s && dest)
+	{
+		while (index != start)
+			index++;
+		start = 0;
+		while (start != len)
+		{
+			dest[start] = s[index];
+			index++;
+			start++;
+		}
+		dest[index] = '\0';
+		return (dest);
+	}
+	return(NULL);
+}
